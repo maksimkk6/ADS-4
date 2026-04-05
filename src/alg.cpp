033b7bec71bc2fe4fca4257330944351ca3d5671
maksimkk6 <konnovmaks@bk.ru>
2026-04-05 22:22:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,107 +1,98 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value)
-{
+int countPairs1(int *arr, int len, int value) {
     int count = 0;
-
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = i + 1; j < len; j++)
-        {
-            if (arr[i] + arr[j] == value)
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
                 count++;
+                while (j + 1 < len && arr[j + 1] == arr[j]) j++;
+            }
         }
+        while (i + 1 < len && arr[i + 1] == arr[i]) i++;
     }
     return count;
 }
 
-int countPairs2(int *arr, int len, int value)
-{
+int countPairs2(int *arr, int len, int value) {
+    int count = 0;
     int left = 0;
     int right = len - 1;
-    int count = 0;
-
-    while (left < right)
-    {
+    
+    while (left < right) {
         int sum = arr[left] + arr[right];
-
-        if (sum == value)
-        {
-            if (arr[left] == arr[right])
-            {
-                int k = right - left + 1;
-                count += k * (k - 1) / 2;
-                break;
-            }
-
-            int l_val = arr[left], r_val = arr[right];
-            int l_count = 0, r_count = 0;
-            while (left < len && arr[left] == l_val)
-            {
-                l_count++;
+        
+        if (sum == value) {
+            int leftCount = 1;
+            while (left + 1 < right && arr[left + 1] == arr[left]) {
+                leftCount++;
                 left++;
             }
-
-            while (right >= 0 && arr[right] == r_val)
-            {
-                r_count++;
+            
+            int rightCount = 1;
+            while (right - 1 > left && arr[right - 1] == arr[right]) {
+                rightCount++;
                 right--;
             }
-            count += l_count * r_count;
+            
+            count += leftCount * rightCount;
+            left++;
+            right--;
         }
-        else if (sum < value)
-        {
+        else if (sum < value) {
             left++;
         }
-        else
-        {
+        else {
             right--;
         }
     }
+    
     return count;
 }
 
-int binarySearch(int *arr, int left, int right, int target)
-{
-    while (left <= right)
-    {
-        int mid = (left + right) / 2;
-
-        if (arr[mid] == target)
+int binarySearch(int *arr, int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            while (mid > left && arr[mid - 1] == target) mid--;
             return mid;
-        else if (arr[mid] < target)
+        }
+        else if (arr[mid] < target) {
             left = mid + 1;
-        else
+        }
+        else {
             right = mid - 1;
+        }
     }
-
     return -1;
 }
 
-
-int countPairs3(int *arr, int len, int value)
-{
+int countPairs3(int *arr, int len, int value) {
     int count = 0;
-
-    for (int i = 0; i < len; i++)
-    {
+    int i = 0;
+    
+    while (i < len) {
         int target = value - arr[i];
-
-        int idx = binarySearch(arr, i + 1, len - 1, target);
-
-        if (idx != -1)
-        {
-            int left = idx;
-            int right = idx;
-
-            while (left > i && arr[left - 1] == target)
-                left--;
-
-            while (right < len - 1 && arr[right + 1] == target)
-                right++;
-
-            count += (right - left + 1);
+        if (target < arr[i]) break;
+        
+        int foundIndex = binarySearch(arr, i + 1, len - 1, target);
+        
+        if (foundIndex != -1) {
+            int countFirst = 1;
+            while (i + 1 < len && arr[i + 1] == arr[i]) {
+                countFirst++;
+                i++;
+            }
+            
+            int countSecond = 1;
+            int j = foundIndex;
+            while (j + 1 < len && arr[j + 1] == target) {
+                countSecond++;
+                j++;
+            }
+            
+            count += countFirst * countSecond;
         }
-    }
-
+        i++;
+    }  
     return count;
 }
